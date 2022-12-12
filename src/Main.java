7cda0e6bed346698e185a0b914bee80506e168d8
elek.viktor <elekviktor2003@gmail.com>
2022-12-12 15:14:03 +0100

    Printek, selection

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,25 @@ public class Main {
     public static void main(String[] args)
     {
         int selection;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Valaszd ki a szamot!");
+            System.out.println("1 - HELLO WORLD");
+            System.out.println("2 - INFO");
+            System.out.println("3 - Exit");
+            System.out.println("4 - HELP");
+            selection = sc.nextInt();
+            switch(selection) {
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+            }
+        }
+        while(selection != 3);
     }
 }
